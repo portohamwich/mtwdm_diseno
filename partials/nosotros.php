d063b2e3e7d3531e1d488bd1750ab3abb55ae385
Porto Hamwich <porto@MacBook-Pro-de-Porto.local>
2015-04-19 13:30:17 -0500

    Ajustes de estilo

--- partials/nosotros.php
+++ partials/nosotros.php
@@ -1,34 +1,49 @@
 <article id="nosotros">
     <h1>Nosotros</h1>
 
-    <h2>Arriesgarse a los sueños, puede valer la pena.</h2>
+    <div class="col-40">
 
-    <p>
-        Hace ya un cierto número de años, con un interes de poder servir y cubrir un mercado
-        cuya presencia es muy baja en nuestra ciudad de León, Gto. y ofrecer un agradable sitio
-        para cenar en familia y/o en pareja; nos dimos a la tarea de iniciar este pequeño negocio
-        de antojitos mexicanos.
-    </p>
+    </div>
+    <div class="col-50" style="float: right;">
+        <h2>Arriesgarse a los sueños, puede valer la pena.</h2>
+        <p>
+            Hace ya un cierto número de años, con un interes de poder servir y cubrir un mercado
+            cuya presencia es muy baja en nuestra ciudad de León, Gto. y ofrecer un agradable sitio
+            para cenar en familia y/o en pareja; nos dimos a la tarea de iniciar este pequeño negocio
+            de antojitos mexicanos.
+        </p>
+    </div>
 
-    <p>
-        Si bien los aÒos nos han dado ya una experiencia, siempre mejoramos dia con dia, para ofrecer
-        lo mejor de nosotros.
-    </p>
+    <div class="clearfix"></div>
 
-    <p>
-        Hoy nos mejoramos en el sentido de dar una nueva experiencia en internet, teniendo en
-        consideracion que muchos de nosotros utilizamos un medio electronico para acceder a este mundo
-        buscando lo que necesitamos; si bien promover nuestro menu para su deleite e invitarlos a
-        que nos visiten y pasen una noche agradable.
-    </p>
+    <div class="col-50">
+        <p>
+            Si bien los aÒos nos han dado ya una experiencia, siempre mejoramos dia con dia, para ofrecer
+            lo mejor de nosotros.
+        </p>
 
-    <p>
-        Si bien nos falta mucho por mejorar, esperamos muy pronto tener la satisfacción de dar mejores
-        de recepcion, entrega, confort y sobre todo nuevos platillos para su deleite.
-    </p>
+        <p>
+            Hoy nos mejoramos en el sentido de dar una nueva experiencia en internet, teniendo en
+            consideracion que muchos de nosotros utilizamos un medio electronico para acceder a este mundo
+            buscando lo que necesitamos; si bien promover nuestro menu para su deleite e invitarlos a
+            que nos visiten y pasen una noche agradable.
+        </p>
+    </div>
 
-    <p>
-        No dudes en dejarnos tus comentarios, sugerencias y/o quejas a travÈs de este medio y bien por nuestro
-        sitio en google+
-    </p>
+    <div class="clearfix"></div>
+
+    <div class="col-40"></div>
+    <div class="col-50" style="float: right;">
+        <p>
+            Si bien nos falta mucho por mejorar, esperamos muy pronto tener la satisfacción de dar mejores
+            de recepcion, entrega, confort y sobre todo nuevos platillos para su deleite.
+        </p>
+
+        <p>
+            No dudes en dejarnos tus comentarios, sugerencias y/o quejas a travÈs de este medio y bien por nuestro
+            sitio en google+
+        </p>
+    </div>
+
+    <div class="clearfix"></div>
 </article>
